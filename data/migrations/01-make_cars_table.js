49c18a85397e8010095c248e2810bfec94c8ed11
billydean <billy.goehring@gmail.com>
2022-04-19 09:27:40 -0700

    initial commit

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    // primary key
+    tbl.increments();
+    tbl.string('vin').unique().notNullable();
+    tbl.string('make').notNullable();
+    tbl.string('model').notNullable();
+    tbl.decimal('mileage').notNullable();
+    tbl.string('title')
+    tbl.string('transmission')
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  // "down" drops the entire cars table; it undoes the "up" function
+  return knex.schema.dropTableIfExists('cars');
 };
