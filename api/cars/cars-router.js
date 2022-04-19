5084da7f4c1db4615a53be5cee909fd4ac7ba3a1
billydean <billy.goehring@gmail.com>
2022-04-19 10:38:58 -0700

    3 of 13 (router.get (all))

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -2,10 +2,35 @@ const router = require('express').Router();
 const md = require('./cars-middleware')
 const Cars = require('./cars-model');
 
-router.get()
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Cars.getAll();
+        res.json(cars);
+    } catch (err) {
+        next(err);
+    }
+})
 
-router.get()
+router.get('/:id', (req, res, next) => {
+    try {
 
-router.post()
+    } catch (err) {
+        next(err);
+    }
+})
+
+router.post('/', (req, res, next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    }
+})
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.message,
+    })
+})
 
 module.exports = router;
