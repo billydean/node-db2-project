9f0011603ae802319f2eb01e6709cda279970796
billydean <billy.goehring@gmail.com>
2022-04-19 10:31:13 -0700

    set up router; correctly this time

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,11 @@
-// DO YOUR MAGIC
+const router = require('express').Router();
+const md = require('./cars-middleware')
+const Cars = require('./cars-model');
+
+router.get()
+
+router.get()
+
+router.post()
+
+module.exports = router;
